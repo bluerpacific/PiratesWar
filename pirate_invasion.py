e55c07ec3497bf063915d14ac53f0ea64552a9a3
bluerpacific <yanlj3333@163.com>
2023-04-30 21:54:26 +0800

    init project

---
+++ pirate_invasion.py
@@ -0,0 +1,30 @@
+import sys
+import pygame
+
+from settings import Settings
+
+
+def run_game():
+    # Initialize game and create a screen object.
+    pygame.init()
+    ai_settings = Settings()
+    screen = pygame.display.set_mode(
+        (ai_settings.screen_width, ai_settings.screen_height))
+
+    pygame.display.set_caption("Pirate Invasion")
+
+    # Start the main loop for the game.
+    while True:
+
+        screen.fill(ai_settings.bg_color)
+
+        # Watch for keyboard and mouse events.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+
+
+run_game()
